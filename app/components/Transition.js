e2bae3cd634e4f5ee95b859eaa583fce05be08cd
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2024-04-27 20:54:36 +0100

    useeffect

--- app/components/Transition.js
+++ app/components/Transition.js
@@ -24,7 +24,7 @@ const Transition = ({ children }) => {
       duration: 10,
       repeatType: "mirror",
     });
-  }, []);
+  }, [pathName]);
   return (
     <AnimatePresence mode="wait">
       <motion.div
