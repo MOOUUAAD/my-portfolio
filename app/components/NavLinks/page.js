57ecf980ae64439a0a8c9183abf384c3a2ff1dd7
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2024-04-27 19:48:18 +0100

    remove pathname

--- app/components/NavLinks/page.js
+++ app/components/NavLinks/page.js
@@ -1,15 +1,21 @@
-"use client"
+"use client";
 
-import { usePathname } from 'next/navigation'
-import React from 'react'
+import React from "react";
 import Link from "next/link";
 
-const NavLinks = ({link}) => {
-const pathName= usePathname()
+const NavLinks = ({ link }) => {
+  // const pathName= usePathname()
 
   return (
-    <Link href={link.url} className={`hover:bg-black hover:text-white duration-150 px-4 py-2 rounded-full ${pathName==link.url && "bg-black text-white"}`}>{link.title}</Link>
-  )
-}
+    <Link
+      href={link.url}
+      className={`hover:bg-black hover:text-white duration-150 px-4 py-2 rounded-full ${
+        false && "bg-black text-white"
+      }`}
+    >
+      {link.title}
+    </Link>
+  );
+};
 
-export default NavLinks
+export default NavLinks;
