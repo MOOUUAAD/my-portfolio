9aa7eac77a8b9fdc4ed90d8795fdcc5a8ad70fc8
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2024-04-27 18:16:07 +0100

    add links

--- app/projects/page.js
+++ app/projects/page.js
@@ -26,7 +26,7 @@ const Projects = () => {
             description="Responsive website for movies recommendations"
             image="/images/Movies-Recommendations.png"
             git="https://github.com/MOOUUAAD/Movies-Recommendations-website/blob/main/README.md"
-            link="https://moouuaad.github.io/Movies-Recommendations-website/?#"
+            link="https://movies-recommendations.vercel.app/"
           />
 
           <ProjectCard
@@ -34,6 +34,7 @@ const Projects = () => {
             description="Simple responsive landing page clone for Mercedes-AMG"
             image="/images/Mercedes-AMG.png"
             git="https://github.com/MOOUUAAD/Mercedes-AMG/blob/main/README.md"
+            link="https://mercedes-amg.vercel.app/"
           />
 
           <ProjectCard
