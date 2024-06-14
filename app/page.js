b28988b95eecd1bd1797fca3e1f8af5487526c11
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2024-06-14 14:56:31 +0100

    edit cv

--- app/page.js
+++ app/page.js
@@ -59,7 +59,7 @@ export default function Home() {
                 className={`inline-block rounded-full px-5 py-3 bg-black transition duration-200 font-bold cursor-pointer`}
               >
 
-                <Link className="relative top-0 z-10"  href={'/files/MyCV.pdf'}  target="_blank"  rel="noopener noreferrer" download>Download CV</Link>
+                <Link className="relative top-0 z-10"  href={'/files/MouadElMaslouhiCV.pdf'}  target="_blank"  rel="noopener noreferrer" download>Download CV</Link>
 
               </motion.button>
               <motion.button
