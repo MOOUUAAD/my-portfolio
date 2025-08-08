b39e068a0d8d674e137726349dea15afc592e39d
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2025-08-09 00:51:45 +0100

    add aws 3 tier web app project

--- app/projects/page.js
+++ app/projects/page.js
@@ -9,15 +9,23 @@ const Projects = () => {
       className="h-full overflow-y-scroll"
       initial={{ y: "-200vh" }}
       animate={{ y: "0%" }}
-      transition={{ delay:0.5, duration: 1 }}
+      transition={{ delay: 0.5, duration: 1 }}
     >
       <div id="projects" className="">
         <div class="flex flex-col md:mx-40 gap-12 justify-center items-center my-6">
           <ProjectCard
+            title="Three-Tier Web App on AWS"
+            description="A fully functional, scalable Three-Tier Web Application by integrating a variety of powerful AWS services"
+            image="/images/3-tier-app.png"
+            git="https://github.com/MOOUUAAD/aws-three-tier-app/blob/main/README.md"
+            link=""
+          />
+
+          <ProjectCard
             title="Association NIA"
             description="Responsive website for association NIA with a landing page and a health chatbot page"
             image="/images/1.png"
-            git="https://github.com/MOOUUAAD/Health-Chatbot-NIA"
+            git="https://github.com/MOOUUAAD/Health-Chatbot-NIA/blob/main/README.md"
             link="https://associaionnia.vercel.app/"
           />
 
@@ -25,7 +33,7 @@ const Projects = () => {
             title="Movies Recommendation"
             description="Responsive website for movies recommendations"
             image="/images/Movies-Recommendations.png"
-            git="https://github.com/MOOUUAAD/Movies-Recommendations-website"
+            git="https://github.com/MOOUUAAD/Movies-Recommendations-website/blob/main/README.md"
             link="https://movies-recommendations.vercel.app/"
           />
 
@@ -33,7 +41,7 @@ const Projects = () => {
             title="Mercedes-AMG"
             description="Responsive landing page clone for Mercedes-AMG"
             image="/images/Mercedes-AMG.png"
-            git="https://github.com/MOOUUAAD/Mercedes-AMG"
+            git="https://github.com/MOOUUAAD/Mercedes-AMG/blob/main/README.md"
             link="https://mercedes-amg.vercel.app/"
           />
 
