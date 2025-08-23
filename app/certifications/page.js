5c1bfabb5df9d132729ebd83d96f77ef6ae83b0e
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2025-08-23 01:41:18 +0100

    CertifCard

--- app/certifications/page.js
+++ app/certifications/page.js
@@ -15,7 +15,7 @@ const Certifications = () => {
         <div class="flex flex-col md:flex-row mx-10 md:mx-20 gap-10 my-6">
           <FeatureCard
             logoImage="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/BCG_Corporate_Logo.jpg/600px-BCG_Corporate_Logo.jpg?20181127202412"
-            title="Digital Transformation"
+            title="Digital"
             hoverColor="#147B58"
             onCredentialsClick={() =>
               window.open("https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/SKZxezskWgmFjRvj9/cT6tMKSaMryk6PGaF_SKZxezskWgmFjRvj9_78ii8yWqSBSdo6bLf_1754767893106_completion_certificate.pdf", "_blank")
